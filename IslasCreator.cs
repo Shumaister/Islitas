7050e9a538775b724889835cc50cc1fcc6cbfdf6
Gabriel Maiori <gabrielmaiori@gmail.com>
2024-04-27 19:06:15 -0300

    first commit

---
+++ IslasCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Islitas;
+
+public static class IslasCreator
+{
+    public static int[,] MapLevel1()
+    {
+        var rnd = new Random();
+
+        int[,] mapa = new int[10, 10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                mapa[i, j] = rnd.Next(10, 20) < 15 ? 1 : 0;
+            }
+        }
+
+        return mapa;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Islitas;
+
+Console.WriteLine("Hello, World!");
+
+var map = IslasCreator.MapLevel1();
+
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        Console.Write(map[i, j]);
+    }
+    Console.WriteLine();
+}
